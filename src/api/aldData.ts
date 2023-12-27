d13730f2dfc345192902b5969115fc62c50c4a29
Solyi Park <itsoholy21@gmail.com>
2023-12-27 17:18:12 +0900

    test2

--- src/api/aldData.ts
+++ src/api/aldData.ts
@@ -15,7 +15,7 @@ type BooksResponse = {
 
 
 const getSearchData = async () => {
-  const url = `https://www.aladin.co.kr/ttb/api/ItemSearch.aspx?ttbkey=`;
+  const url = `/ttb/api/ItemSearch.aspx?ttbkey=`;
   try {
     // Query=[여기에 검색어 들어감]
     //axios.get 메서드는 제네릭 타입을 받음
@@ -29,7 +29,7 @@ const getSearchData = async () => {
   }
 };
 const getItemData = async () => {
-  const url = `https://www.aladin.co.kr/ttb/api/ItemLookUp.aspx?ttbkey=`;
+  const url = `/ttb/api/ItemLookUp.aspx?ttbkey=`;
   try {
     //ItemId=[여기에isbn 넣어줘야함]
     const resp = await axios.get(`${url}${process.env.REACT_APP_ALD_API_KEY}&itemIdType=ISBN&ItemId=K962936839&output=js&Version=20131101&OptResult=ebookList,usedList,reviewList

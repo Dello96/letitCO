dcabd4debddf2c371e7cc9c4b24d8a92644d20e2
loveytheb <146186897+loveytheb@users.noreply.github.com>
2023-12-30 16:36:31 +0900

    home 수정 중

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -17,12 +17,22 @@ import {
   StReadingMemo,
   StReadingMemoIndex
  } from './style'
+import { SupabaseClient } from '@supabase/supabase-js'
+import { getMemos, getBooks } from '../../api/supabaseData'
+import { QUERY_KEYS } from '../../query/keys'
+import { useQuery } from 'react-query';
 
 export default function Home() {
+  // 읽은 페이지 표시
   // const [currentPage, setCurrentPage] = React.useState<number>(0);
   // const totalPage = data?.length || 0;
   // const progressPercentage = ((currentPage + 1) / totalPage) * 100;
 
+  const {isLoading, data: books} = useQuery({
+    queryKey: [QUERY_KEYS.BOOKS],
+    queryFn: getBooks,
+  });
+  // const memo = memos?.find((memo) => memo.id === memo.id)
 
   return (
     <>

29dbc01e9d7b7a2057545d0f585dcbf9f28a6222
Solyi Park <itsoholy21@gmail.com>
2024-01-03 06:04:06 +0900

    테스트

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from '../pages/Home';
 import Login from '../../src/pages/login/Login';
@@ -10,31 +10,31 @@ import Calendar from '../pages/calendar';
 // import { useSelector, useDispatch } from 'react-redux';
 // import { RootState } from '../redux/store';
 import Layout from '../components/Layout';
-import { useDispatch } from 'react-redux';
-import { setUser } from '../redux/userSlice';
+
 
 const Router = () => {
-  const dispatch = useDispatch();
-
-  const authTokenStr = localStorage.getItem('sb-bsnozctogedtgqvbhqby-auth-token');
-
+  const [user, setUser] = useState('')
+  useEffect(() => {
+    const authTokenStr = localStorage.getItem('sb-bsnozctogedtgqvbhqby-auth-token');
   if (authTokenStr) {
     //제이슨으로 바꾸기
     const authToken = JSON.parse(authTokenStr);
+    console.log('authToken=========>', authToken)
     const userId: string = authToken.user.id;
-    const user = {
-      id: userId
-    }
     console.log('사용자 ID:', userId);
-    dispatch(setUser(user));
+    setUser(userId)
+
   } else {
     console.log('Auth 토큰을 찾을 수 없습니다.');
   }
+}, [])
+ 
+
 
   return (
     <BrowserRouter>
       <Routes>
-        {/* {currentUser.id ? ( */}
+        {user ? (
         <>
           <Route element={<Layout />}>
             <Route path="/" element={<Home />} />
@@ -47,12 +47,12 @@ const Router = () => {
             <Route path="*" element={<Navigate to="/" replace />} />
           </Route>
         </>
-        {/* ) : ( */}
+         ) : ( 
         <>
           <Route path="/login" element={<Login />} />
           <Route path="*" element={<Navigate to="/login" replace />} />
         </>
-        {/* )} */}
+         )} 
       </Routes>
     </BrowserRouter>
   );
